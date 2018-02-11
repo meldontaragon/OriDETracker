f652172ec62b6a71e1c2eb4bfaf80193773dedfe
David Miller <david.cornelius.miller@gmail.com>
2018-02-11 17:04:46 -0500

    Nearly have 1st/2nd place working

--- WebVersion/allskills/client.js
+++ WebVersion/allskills/client.js
@@ -1,6 +1,68 @@
 var matchId,
     intervalHandle;
 
+function onRadioChange()
+{
+    var id1 = "radio-t1-place-first",
+	imgId1 = id1.substr( 6 ),
+	state1 = document.getElementById(id1).checked,
+	id2 = "radio-t2-place-first",
+	imgId2 = id2.substr( 6 ),
+	state2 = document.getElementById(id2).checked,
+	data = {
+	    match: matchId,
+	    state: {
+	    }
+	};
+
+    if ( state1 )
+    {
+	$( '#' + imgId1 ).addClass( 'show' );
+	$( '#' + imgId1 + '-g' ).removeClass( 'show' );
+    }
+    else
+    {
+	$( '#' + imgId1 ).removeClass( 'show' );
+	$( '#' + imgId1 + '-g' ).addClass( 'show' );
+    }
+
+    if ( state2 )
+    {
+	$( '#' + imgId2 ).addClass( 'show' );
+	$( '#' + imgId2 + '-g' ).removeClass( 'show' );
+    }
+    else
+    {
+	$( '#' + imgId2 ).removeClass( 'show' );
+	$( '#' + imgId2 + '-g' ).addClass( 'show' );
+
+    }
+
+    data.state[imgId1] = state1;
+    $.post( 'server.php', data, function ( res ) {
+	$( '#sync-message' )
+	    .text( 'First part sent successfully.' )
+	    .fadeIn( 50 )
+	    .delay( 3000 )
+	    .fadeOut( 1000 );
+    } );
+
+    var data2 = {
+	match: matchId,
+	state: {
+	}
+    };
+    
+    data2.state[imgId2] = state2;
+    $.post( 'server.php', data2, function ( res ) {
+	$( '#sync-message' )
+	    .text( 'Second part sent successfully.' )
+	    .fadeIn( 50 )
+	    .delay( 3000 )
+	    .fadeOut( 1000 );
+    } );
+}
+
 function onCheckboxChange() {
     // id is of format `<t1|t2>-<ID of corresponding image>`
     var id = this.id,
@@ -12,6 +74,33 @@ function onCheckboxChange() {
 	    }
 	};
 
+    /*
+    //check if it's t1-place-first or t2-place-first
+    if ( this.id == 'check-t1-place-first' )
+    {
+	//document.getElementById('check-t2-place-first').checked = !(document.getElementById('check-t1-place-first').checked);
+	var state2 = !(document.getElementById('check-t1-place-first').checked),
+	    imgId2 = 'check-t2-place-first'.substr(6);
+
+	data.state[imgId2] = state2;
+
+	$( '#' + imgId2 ).toggleClass( 'show' );
+	$( '#' + imgId2 + '-g' ).toggleClass( 'show' );
+    }
+
+    
+    if ( this.id == 'check-t2-place-first' )
+    {
+	//document.getElementById('check-t1-place-first').checked = !(document.getElementById('check-t2-place-first').checked);
+	var state2 = !(document.getElementById('check-t2-place-first').checked),
+	    imgId2 = 'check-t1-place-first'.substr(6);
+
+	data.state[imgId2] = state2;
+
+	$( '#' + imgId2 ).toggleClass( 'show' );
+	$( '#' + imgId2 + '-g' ).toggleClass( 'show' );
+    }
+    */
     data.state[imgId] = state;
     $( '#' + imgId ).toggleClass( 'show' );
     $( '#' + imgId + '-g' ).toggleClass( 'show' );
@@ -30,7 +119,7 @@ function updateState( state ) {
 
     // We want to find everything that's currently shown EXCEPT
     // those images which are shown *by default* since we're going
-    // to check these results agains the provided state and hide
+    // to check these results against the provided state and hide
     // any that don't match the server.
     $( '.show' ).not( '[id$="-g"]' ).each( function ( _, $el ) {
 	if ( ids.indexOf( $el.id ) < 0 ) {
@@ -64,7 +153,7 @@ function linkMatch( e ) {
 		.text( 'Match not found. New match created.' )
 		.css( 'color', 'black' )
 		.fadeIn( 50 );
-
+	    //this should also "link" it now
 	    return;
 	}
 	updateState( res );
@@ -87,3 +176,4 @@ function linkMatch( e ) {
 
 $( 'body' ).on( 'change', 'input[type="checkbox"]', onCheckboxChange );
 $( '#matchForm' ).on( 'submit', linkMatch );
+$( 'body' ).on( 'change', 'input[type="radio"]', onRadioChange );
