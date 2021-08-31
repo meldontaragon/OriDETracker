afae196c08b1cbe134d8066412c2d3ad873efdf6
jeflefou <jeflefou@gmail.com>
2021-08-31 20:26:46 +0200

    Push to version v3.3.0 for beta testing

--- OriDETracker/Properties/AssemblyInfo.cs
+++ OriDETracker/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("MeldonTaragon")]
 [assembly: AssemblyProduct("OriDETracker")]
-[assembly: AssemblyCopyright("Copyright ©  2017-2019")]
+[assembly: AssemblyCopyright("Copyright ©  2017-2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.1.*")]
-[assembly: AssemblyVersion("3.2.0.*")]
-[assembly: AssemblyFileVersion("3.2.0")]
+[assembly: AssemblyVersion("3.3.0.*")]
+[assembly: AssemblyFileVersion("3.3.0")]
