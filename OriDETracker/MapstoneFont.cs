cf3214b058663bdf94429f696e4824f2303daace
jeflefou <jeflefou@gmail.com>
2021-09-04 20:02:51 +0200

    Optimized drawing. Introduce MapstoneFontFactory to build mapstone font/text only when needed (tracker size change, font change, font color change). UpdateGraphics was creating a Font on every update.

---
+++ OriDETracker/MapstoneFont.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+
+namespace OriDETracker
+{
+    internal class MapstoneFont : IDisposable
+    {
+        internal Font Font { get; }
+        internal Brush Brush { get; }
+        internal Point Location { get; }
+
+        internal MapstoneFont(Font font, Brush brush, Point location)
+        {
+            Font = font;
+            Brush = brush;
+            Location = location;
+        }
+
+        public void Dispose()
+        {
+            Font?.Dispose();
+        }
+    }
+}
--- OriDETracker/MapstoneText.cs
+++ OriDETracker/MapstoneText.cs
@@ -1,17 +1,16 @@
 namespace OriDETracker
 {
-    class MapstoneText
+    internal class MapstoneText
     {
-        public int X;
-        public int Y;
-        public int TextSize;
+        public int X { get; }
+        public int Y { get; }
+        public int Size { get; }
 
-        public MapstoneText(int a, int b, int size)
+        public MapstoneText(int x, int y, int size)
         {
-            X = a;
-            Y = b;
-            TextSize = size;
+            X = x;
+            Y = y;
+            Size = size;
         }
     }
-
 }
